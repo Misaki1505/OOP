b7e88c55b60c2cd410918ca2108164cc36e58a28
Misaki1505 <sawako1505@gmail.com>
2022-12-20 19:34:48 -0500

    Changes 20122022

--- app/src/main/java/com/example/oop/Classes.kt
+++ app/src/main/java/com/example/oop/Classes.kt
@@ -1,5 +1,28 @@
 package com.example.oop
 
-fun main(string: String) {
+fun main(args: Array<String>) {
+    val myCar: Car = Car()
+    val yourCar = Car()
 
+    myCar.model = "BMW"
+    yourCar.model = "Toyota"
+
+    myCar.start()
+    myCar.drive(50)
+
+    yourCar.start()
+    yourCar.drive(75)
+}
+
+class Car {
+    var model: String? = null
+    var topSpeed = 100
+
+    fun start() {
+        println("Starting the $model")
+    }
+
+    fun drive(speed: Int) {
+        println("Driving at a speed to $topSpeed")
+    }
 }
