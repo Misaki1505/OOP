fa5b14d4dc86bc0963ca913a96b615a8484bfa3e
Misaki1505 <sawako1505@gmail.com>
2022-12-25 18:28:00 -0500

    Changes 25122022

--- app/src/main/java/com/example/oop/OOP4.kt
+++ app/src/main/java/com/example/oop/OOP4.kt
@@ -1,5 +1,45 @@
 package com.example.oop
 
 fun main(args: Array<String>) {
+    val calc = Calculator()
+    calc.add(47.0)
+    calc.mult(15.0)
+    calc.div(23.0)
+    calc.sub(135.7645)
+    calc.reset()
+    calc.add(23.0)
 
+}
+
+class Calculator {
+    var total = 0.0
+
+    fun add(a: Double) {
+        total += a
+        println("+ $a")
+        println("= $total")
+    }
+
+    fun sub(a: Double) {
+        total -= a
+        println("- $a")
+        println("= $total")
+    }
+
+    fun div(a: Double) {
+        total /= a
+        println("/ $a")
+        println("= $total")
+    }
+
+    fun mult(a: Double) {
+        total *= a
+        println("* $a")
+        println("= $total")
+    }
+
+    fun reset(){
+        total = 0.0
+        println("= $total")
+    }
 }
