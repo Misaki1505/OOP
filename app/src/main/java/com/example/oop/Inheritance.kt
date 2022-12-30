a30572fcd120f1fdcc1b1f51f02993d0a3f0bf78
Misaki1505 <sawako1505@gmail.com>
2022-12-29 20:06:46 -0500

    Changes 29122022

--- app/src/main/java/com/example/oop/Inheritance.kt
+++ app/src/main/java/com/example/oop/Inheritance.kt
@@ -1,5 +1,25 @@
 package com.example.oop
 
 fun main(args: Array<String>) {
+    val myDog = Corgi()
+    myDog.size = 10
+    println(myDog.size)
+    myDog.bark()
+    myDog.play()
+}
+
+open class Dog {
+    var size = 0
+
+    fun bark() {
+        println("Bark")
+    }
+
+    fun play() {
+        println("Play")
+    }
+}
+
+class Corgi: Dog() {
 
 }
